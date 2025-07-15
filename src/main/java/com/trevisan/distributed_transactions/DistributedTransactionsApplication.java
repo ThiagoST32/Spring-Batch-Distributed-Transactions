8943b7f0f0ecb0d1ea820e30e0d1eaa06113adc6
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2025-07-14 21:19:36 -0400

    Initial files

---
+++ src/main/java/com/trevisan/distributed_transactions/DistributedTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.trevisan.distributed_transactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedTransactionsApplication.class, args);
+	}
+
+}
