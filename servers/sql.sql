c6e6c3840fee587402e67b7a9ca1f8ea3a514e39
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2025-07-14 23:31:12 -0400

    feat(servers) Adding configuration files.

---
+++ servers/sql.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE IF NOT EXISTS data01;
+USE data01;
+CREATE TABLE pessoa(name varchar(255), document varchar(255), email varchar(255), phone varchar(255), age varchar(255));
+
+CREATE DATABASE IF NOT EXISTS data02;
+USE data02;
+CREATE TABLE pessoa(name varchar(255), document varchar(255), email varchar(255), phone varchar(255), age varchar(255));
